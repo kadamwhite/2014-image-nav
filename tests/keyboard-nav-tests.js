df33625db468351ed87d7e968f14015514b3a2d3
K.Adam White <kadamwhite@gmail.com>
2013-11-28 16:04:59 -0600

    Add tests for default keyboard navigation behavior
    We can't directly test redirection via window.location, so I've created a
    TwentyFourteen namespace and moved the actual navigation call into a new
    `TwentyFourteen.navigate` method. This allows us to stub out the navigation
    itself within the tests, to verify that everything else works as expected.

--- tests/keyboard-nav-tests.js
+++ tests/keyboard-nav-tests.js
@@ -1,8 +1,69 @@
 (function( $ ) {
 	'use strict';
 
-	test( 'Basic test example from qunitjs.com', function() {
-		ok( 1 == '1', 'Loose equality works as expected!' );
+	var $document, mockEvent;
+
+	// Group navigation tests in a module: This allows us to define setup and
+	// teardown methods to reduce code duplication between tests.
+	module( 'Basic Keyboard Navigation', {
+		// Run before each test
+		setup: function () {
+			// Re-usable jQuery reference to the document object
+			$document = $( document );
+
+			// Observe TwentyFourteen.navigate, but do not call through
+			sinon.stub( TwentyFourteen, 'navigate' );
+
+			// Create a mock event to trigger navigation
+			mockEvent = $.Event( 'keydown.twentyfourteen' );
+		},
+		// Run after each test
+		teardown: function() {
+			// Restore the original TwentyFourteen.navigate method
+			TwentyFourteen.navigate.restore();
+		}
+	});
+
+	test( 'Left key navigates to previous image', function() {
+		// Set event to be the left arrow key code
+		mockEvent.which = 37;
+
+		// Trigger event
+		$document.trigger( mockEvent );
+
+		// Expect location to have been called with the URL from the fixture
+		ok( TwentyFourteen.navigate.calledWith( 'prev-image-link.html' ),
+			'Navigation called with the URL for the next page' );
+	});
+
+	test( 'Right key navigates to next image', function() {
+		// Set event to be the left arrow key code
+		mockEvent.which = 39;
+
+		// Trigger event
+		$document.trigger( mockEvent );
+
+		// Expect location to have been called with the URL from the fixture
+		ok( TwentyFourteen.navigate.calledWith( 'next-image-link.html' ),
+			'Navigation called with the URL for the next page' );
+	});
+
+	test( 'No navigation occurs if textarea or input have focus', function() {
+		// Force $( 'textarea, input' ).is( ':focus' ) to return "true"
+		sinon.stub( $.fn, 'is', function() {
+			return true;
+		});
+
+		// Trigger a left-arrow event
+		mockEvent.which = 37;
+		$document.trigger( mockEvent );
+
+		// Nothing should have happened
+		ok( ! TwentyFourteen.navigate.called,
+			'No navigation should occur when an input element is focused' );
+
+		// Restore jQuery.fn.is
+		$.fn.is.restore();
 	});
 
 })( jQuery );
