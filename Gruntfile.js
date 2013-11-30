818685a6bc054d68b2db40f47d31ee70b4dd52b1
K.Adam White <kadamwhite@gmail.com>
2013-11-30 03:08:43 -0600

    Add Grunt tasks to run tests against the minified code
    This follows the general model used by WordPress Core to test minified code:
    
    1. Copy the existing QUnit file, modifying it to point to the minified scripts instead of the regular ones
    2. Run the same QUnit suite against both versions of the test HTML file

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,13 +2,31 @@
 module.exports = function(grunt) {
 	// Project configuration.
 	grunt.initConfig({
+		clean: {
+			build: ['js/keyboard-image-navigation.min.js']
+		},
+		copy: {
+			// Copy the main QUnit file and point the copy at the compiled plugin script
+			qunit: {
+				src: 'tests/qunit-tests.html',
+				dest: 'tests/qunit-compiled.html',
+				options: {
+					processContent: function( src ) {
+						return src.replace( 'keyboard-image-navigation.js' , 'keyboard-image-navigation.min.js' );
+					}
+				}
+			}
+		},
 		jshint: {
 			options: grunt.file.readJSON( '.jshintrc' ),
 			grunt: {
 				src: ['Gruntfile.js']
 			},
 			logic: {
-				src: ['js/**/*.js']
+				src: [
+					'js/**/*.js',
+					'!js/**/*.min.js'
+				]
 			},
 			tests: {
 				src: ['tests/**/*.js'],
@@ -17,23 +35,42 @@ module.exports = function(grunt) {
 		},
 		qunit: {
 			// Run any HTML files found in the /tests directory through QUnit
-			all: ['tests/qunit-tests.html']
+			code: ['tests/qunit-tests.html'],
+			build: ['tests/qunit-compiled.html']
+		},
+		uglify: {
+			build: {
+				src: 'js/keyboard-image-navigation.js',
+				dest: 'js/keyboard-image-navigation.min.js'
+			}
 		},
 		watch: {
 			logic: {
-				files: ['js/**'],
-				tasks: ['jshint:logic', 'qunit']
+				files: ['js/**', '!*.min.js'],
+				tasks: ['jshint:logic', 'qunit:code']
 			},
 			tests: {
 				files: ['tests/**'],
-				tasks: ['jshint:tests', 'qunit']
+				tasks: ['jshint:tests', 'qunit:code']
 			}
 		}
 	});
 
+	grunt.loadNpmTasks( 'grunt-contrib-clean' );
+	grunt.loadNpmTasks( 'grunt-contrib-copy' );
 	grunt.loadNpmTasks( 'grunt-contrib-jshint' );
 	grunt.loadNpmTasks( 'grunt-contrib-qunit' );
+	grunt.loadNpmTasks( 'grunt-contrib-uglify' );
 	grunt.loadNpmTasks( 'grunt-contrib-watch' );
 
-	grunt.registerTask( 'default', [ 'jshint', 'qunit' ] );
+	grunt.registerTask( 'test', ['qunit:code'] );
+
+	grunt.registerTask( 'default', [
+		'jshint',
+		'qunit:code',
+		'clean',
+		'uglify',
+		'copy',
+		'qunit:build'
+	]);
 };
